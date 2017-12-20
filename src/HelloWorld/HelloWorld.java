2cdd0508172790949abbffcc1ddf585599845936
Lasha <lashukino@gmail.com>
2017-12-20 19:33:07 +0400

    testing

--- src/HelloWorld/HelloWorld.java
+++ src/HelloWorld/HelloWorld.java
@@ -2,6 +2,6 @@ package HelloWorld;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("Hello World");
     }
 }
